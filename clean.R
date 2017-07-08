a4af6ad424782af53eaf9546a45747a470f3a4d6
jimmygzp <zg2171@columbia.edu>
2017-07-07 22:23:05 -0400

    updatd

--- clean.R
+++ clean.R
@@ -2,6 +2,13 @@ dirty_data = read.csv("dirty_data.csv")
 myvars <- names(dirty_data)%in%c("Street", "Strange.HTML")
 clean_data = dirty_data[!myvars]
 library(stringi)
+clean_data$Street.2 = stri_trans_totitle(clean_data$Street.2)
+
+Encoding(clean_data$Street.2) <- "latin1"
+clean_data$Street.2 = iconv(clean_data$Street.2, "latin1", "ASCII", sub="")
+
+
 clean_data$Area = sub("^$", "Unknown", clean_data$Area)
+clean_data$Street.2 = trimws(clean_data$Street.2)
 clean_data = unique(clean_data)
 write.csv(clean_data, file = "clean_data.csv")
